4267fe8d9a4008cef70631bfa713d45594993531
ShahzodTuraev <turayevshahzodbek@gmail.com>
2022-11-06 18:48:30 +0900

    third lesson actions

--- src/state.jsx
+++ src/state.jsx
@@ -5,29 +5,44 @@ class App extends React.Component {
     super(props);
     this.state = {
       count: 0,
-      title: 'Web brain'
+      title: 'Web brain',
+      gender: 'das'
 
     }
   }
   render() {
-    console.log('render');
-    const plus = () => {
-      if (this.state.count < 10) this.setState({ count: this.state.count + 1 });
-    };
-    const minus = () => {
-      if (this.state.count > 1) this.setState({ count: this.state.count - 1 });
-    };
-    const onChange = (event) => {
+    // console.log('render');
+    // const plus = () => {
+    //   if (this.state.count < 10) this.setState({ count: this.state.count + 1 });
+    // };
+    // const minus = () => {
+    //   if (this.state.count > 1) this.setState({ count: this.state.count - 1 });
+    // };
+    const onChangef =(event)=> {
       console.log(event.target.value);
       this.setState({title: event.target.value})
     }
+    const onChangee =(events)=>{
+      console.log(events.target.value);
+      this.setState({gender: events.target.value})
+    }
+    const onCheck =(eventch)=>{
+      console.log(eventch.target.checked);
+      this.setState()
+    }
     return (
       <div>
-        <h1>{this.state.title}</h1>
-        <h1>State {this.state.count}</h1>
-        <input onChange={onChange} type="text"  placeholder='name'/>
-        <button onClick={plus}>+</button>
-        <button onClick={minus}>-</button>
+        <input onChange={onChangef} type="text" placeholder='name' />
+         <h1>{this.state.title}</h1>
+       {/* <h1>State {this.state.count}</h1>
+        <button  >+</button>
+        <button onClick={minus}>-</button> */}
+        <select onChange={onChangee} name="" id="">
+          <option value="male">male</option>
+          <option value="female">female</option>
+        </select>
+        <h2>{this.state.gender}</h2>
+        <input onChange={onCheck} type="checkbox" />
       </div>
     )
   }
