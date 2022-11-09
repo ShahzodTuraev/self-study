bb91fcf05333c70abc9956721548e6b89256677f
ShahzodTuraev <turayevshahzodbek@gmail.com>
2022-11-09 14:50:14 +0900

    lesson 4 extra

--- src/state.jsx
+++ src/state.jsx
@@ -1,88 +1,65 @@
 import React from 'react';
-import {student} from './mock'
+// import {student} from './mock'
 
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
       count: 0,
+      isTrue: true
       // title: 'Web brain',
       // gender: 'das',
-      data: student,
-      name: 'Shahzod',
-      surname: 'Turaev'
+      // data: student,
+      // name: 'Shahzod',
+      // surname: 'Turaev'
     }
   }
   render() {
-    const onFilter = (e) => {
-      console.log(e.target.value);
-      let res = student.filter(value=> value.status.includes(e.target.value))
-      this.setState({
-        data: res
-      })
-
+    const onChange =({target: {value, name }}) => {
+     this.setState({[name] : value});
+    //  this.setState({isTrue: this.state.password?.length <= 4})
+    this.setState({[name] : value}, () => {
+      console.log(this.state.password?.length);
+    })
     }
-
-    // console.log('render');
-    // const plus = () => {
-    //   if (this.state.count < 10) this.setState({ count: this.state.count + 1 });
-    // };
-    // const minus = () => {
-    //   if (this.state.count > 1) this.setState({ count: this.state.count - 1 });
-    // };
-    // const onChangef =(event)=> {
-    //   console.log(event.target.value);
-    //   this.setState({title: event.target.value})
-    // }
-    // const onChangee =(events)=>{
-    //   console.log(events.target.value);
-    //   this.setState({gender: events.target.value})
-    // }
-    // const onCheck =(eventch)=>{
-    //   console.log(eventch.target.checked);
-    //   this.setState()
-    // }
-
-    // const onChangem =(event)=> {
-    //   this.setState({[event.target.name]: event.target.value})
-    // }
-    // const onChangeName =(e)=>{
-    //   // console.log(e);
-    //   console.log(e.target.value);
-    //   this.setState({name: e.target.value})
-    // }
-    // const onChangeSurname =(e)=>{
-    //   console.log(e.target.value);
-    //   this.setState({surname: e.target.value})
-    // }
     
-    return (
+    return(
       <div>
-         {/* <h1>name: {this.state.name}</h1>
-         <h1>surname: {this.state.surname}</h1>
-        <input name='name' onChange={onChangem} type="text" placeholder='name' />
-        <input name='surname' onChange={onChangem} type="text" placeholder='surname' />
-       <h1>State {this.state.count}</h1>
-        <button  >+</button>
-        <button onClick={minus}>-</button>
-        <select onChange={onChangee} name="" id="">
-          <option value="male">male</option>
-          <option value="female">female</option>
-        </select>
-        <h2>{this.state.gender}</h2>
-        <input onChange={onCheck} type="checkbox" /> */}
+        <h1>Number : {this.state.password} - 
+        { this.state.isTrue ? "to'g'ri" : "noto'g'ri"}</h1>
+        <br />
 
-        <input onChange={onFilter} type="text"  placeholder='filter'/>
-
-        {
-          this.state.data.map(({id, name, status})=>{
-            return(
-              <h1 key={id}>{id} {name} {status}</h1>
-            )
-          })
-        }
+        <input name='password' type="number" onChange={onChange} />
       </div>
     )
+
+
+
+    // const onFilter = (e) => {
+    //   console.log(e.target.value);
+    //   let res = student.filter(value=> value.status.includes(e.target.value))
+    //   this.setState({
+    //     data: res
+    //   })
+
+    // }
+
+    
+    // return (
+    //   <div>
+         
+
+    //     <input onChange={onFilter} type="text"  placeholder='filter'/>
+
+    //     {
+    //       this.state.data.map(({id, name, status})=>{
+    //         return(
+    //           <h1 key={id}>{id} {name} {status}</h1>
+    //         )
+    //       })
+    //     }
+    //   </div>
+    // )
   }
 }
 export default App;
